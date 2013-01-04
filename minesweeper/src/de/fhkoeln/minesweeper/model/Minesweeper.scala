25376d792bc50b32a935b49425db74c321c5c349
ti614 <ti614@LWIP12.GM-AD.GM.FH-Koeln.DE>
2013-01-04 16:05:46 +0100

    Text UI and Main class are added as dummy version to the repository

---
+++ minesweeper/src/de/fhkoeln/minesweeper/model/Minesweeper.scala
@@ -0,0 +1,14 @@
+package de.fhkoeln.minesweeper.model
+
+import de.fhkoeln.minesweeper.view.TUI
+
+object Minesweeper {
+  
+  val tui = new TUI
+  
+  def main(args: Array[String]) {
+
+    while (tui.processInputLine(readLine())) {}
+  }
+
+}
