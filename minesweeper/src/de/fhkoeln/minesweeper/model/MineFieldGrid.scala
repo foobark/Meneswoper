aa5fe6c53d66441f9c5503a78cf9a47919149da2
daniel <nightmare666_online@yahoo.de>
2013-01-04 01:00:01 +0100

    Cleaned Up Formatting

--- minesweeper/src/de/fhkoeln/minesweeper/model/MineFieldGrid.scala
+++ minesweeper/src/de/fhkoeln/minesweeper/model/MineFieldGrid.scala
@@ -2,116 +2,117 @@ package de.fhkoeln.minesweeper.model
 
 import scala.util.Random
 
-case class MineFieldGrid(val xsize: Int, val ysize: Int, val minecount: Int, val initial_field: (Int, Int) ) {
-	
-	if ( minecount > xsize * ysize) throw new IllegalArgumentException("Too many mines for grid size")
-	
-	if ( minecount < 0) throw new IllegalArgumentException("minecount must be positive")
-	
-	if ( xsize < 0 || ysize < 0 ) throw new IllegalArgumentException("xsize and ysize must be positive")
-	
-	if ( initial_field._1 < 0 || initial_field._2 < 0) throw new IllegalArgumentException("Initial field coordinates must be positive")
+case class MineFieldGrid( val xsize: Int, val ysize: Int, val minecount: Int, val initial_field: ( Int, Int ) ) {
 
-	populateField()
-	
-	//get a copy of the internal grid
-	def getGrid(): Array[Array[MineField]] = grid.clone()
-	
-	/*
+    if ( minecount > xsize * ysize ) throw new IllegalArgumentException( "Too many mines for grid size" )
+
+    if ( minecount < 0 ) throw new IllegalArgumentException( "minecount must be positive" )
+
+    if ( xsize < 0 || ysize < 0 ) throw new IllegalArgumentException( "xsize and ysize must be positive" )
+
+    if ( initial_field._1 < 0 || initial_field._2 < 0 ) throw new IllegalArgumentException( "Initial field coordinates must be positive" )
+
+    populateField()
+
+    //get a copy of the internal grid
+    def getGrid(): Array[ Array[ MineField ] ] = grid.clone()
+
+    /*
 	 * 
 	 * @Return: tupel containing updated grid, Boolean indicating whether a mine was uncovered and finally a Boolean
 	 * if everything besides the mines was uncovered.
 	 */
-	def uncoverField( pos: (Int, Int) ): ( Array[Array[MineField]], Boolean, Boolean ) =  {
-		val field = grid(pos._1)(pos._2)
-		try	field.uncover()
-		catch {
-		  case iae: IllegalArgumentException => throw new MineGridException("Trying to uncover marked field at: " + pos.toString())
-		}
-		field match {
-		  //Uncover armed mine indicate lost game
-		  case MineField(_, true, _, _) => (grid.clone(), true, false)
-		  //If field has adjacent mines simply uncover it
-		  case MineField(adjacent, _, _, _) if adjacent > 0 => (grid.clone(), false, false)
-		  //If field has no adjacent mines uncover all adjacent fields until a field with adjacent mines is discovered
-		  case MineField(0,_,_,_) => {
-		    val positions = getAdjacentPos(pos._1, pos._2)
-		    positions.init foreach(uncoverField)
-		    uncoverField(positions.last) 
-		  }
-		}
-	}
-	
-	//mark the Field at specified position. Throws Exception if said field is already uncovered
-	def markField(pos: (Int, Int)) {
-		try grid(pos._1)(pos._2).marked_=(true)
-		catch {
-			case iae: IllegalArgumentException => throw new MineGridException("Trying to mark uncovered field at: " + pos.toString())
-		}
-	}
-	
-	//unmark the Field at specified position
-	def unmarkField(pos: (Int, Int)) = grid(pos._1)(pos._2).marked = false
-	
-	//setup the mine grid with, i.e. initialize all fields.
-	private def populateField() {
-		//place mines first...
-		placeMines()
-		//...then fill the rest and calculate no. of adjacent mines for fields
-		for( i <- 0 until ysize; j <- 0 until xsize) {
-			//make sure no double placements happen (and mines get overwritten)
-			if (fieldEmpty(i, j)) {
-				grid(i)(j) = MineField(adjacent = countAdjacentMines(i, j))
-			}
-		}
-	}
-	
-	//Position mines randomly on the grid. Avoid initial position
-	private def placeMines() {
-	  var placed = 0
-	  var randy = new Random()
-	  while (placed != minecount) {
-		  val row = randy.nextInt(ysize)
-		  val col = randy.nextInt(xsize)
-		  //make sure same field doesn't get populated twice
-		  if (fieldEmpty(row,col) && (row, col) != initial_field ) {
-			  grid(row)(col) = MineField(armed = true)
-			  placed += 1
-		  }
-	  }
-	}
-	
-	//Count the number of Mines adjacent to a field
-	private def countAdjacentMines(y: Int, x: Int):Int =  getAdjacentPos(y,x) count(isMineField)
-	
-	//get adjacent, valid positions to a field
-	private def getAdjacentPos(y: Int, x:Int): List[(Int, Int)] = {
-			List(
-				 (y + 1, x ),
-				 (y + 1, x -1),
-				 (y, x - 1),
-				 (y-1, x - 1),
-				 (y - 1, x),
-				 (y - 1, x +1),
-				 (y, x + 1),
-				 (y + 1, x + 1)
-			).filter( isValidPos )  
-	}
-	
-	//check whether the field is an armed mine
-	private def isMineField(pos: (Int, Int)):Boolean =  grid(pos._1)(pos._2).armed
-	
-	//Check if position is within boundaries
-	private def isValidPos(pos: (Int, Int)):Boolean = !fieldEmpty(pos._1, pos._2) && yboundaries.contains(pos._1) && xboundaries.contains(pos._2)
-	
-	//check whether the position has already been populated with a field
-	private def fieldEmpty(y: Int, x: Int):Boolean = grid(y)(x) == null
-	
-	private var grid = Array.ofDim[MineField](ysize, xsize)
-	
-	private val xboundaries = 0 to xsize
-	
-	private val yboundaries = 0 to ysize
+    def uncoverField( pos: ( Int, Int ) ): ( Array[ Array[ MineField ] ], Boolean, Boolean ) = {
+        val field = grid( pos._1 )( pos._2 )
+        
+        try field.uncover()
+        catch {
+            case iae: IllegalArgumentException => throw new MineGridException( "Trying to uncover marked field at: " + pos.toString() )
+        }
+        
+        field match {
+            //Uncover armed mine indicate lost game
+            case MineField( _, true, _, _ )                     => ( grid.clone(), true, false )
+            //If field has adjacent mines simply uncover it
+            case MineField( adjacent, _, _, _ ) if adjacent > 0 => ( grid.clone(), false, false )
+            //If field has no adjacent mines uncover all adjacent fields until a field with adjacent mines is discovered
+            case MineField( 0, _, _, _ ) => {
+                val positions = getAdjacentPos( pos._1, pos._2 )
+                positions.init foreach ( uncoverField )
+                uncoverField( positions.last )
+            }
+        }
+    }
+
+    //mark the Field at specified position. Throws Exception if said field is already uncovered
+    def markField( pos: ( Int, Int ) ) {
+        try grid( pos._1 )( pos._2 ).marked_=( true )
+        catch {
+            case iae: IllegalArgumentException => throw new MineGridException( "Trying to mark uncovered field at: " + pos.toString() )
+        }
+    }
+
+    //unmark the Field at specified position
+    def unmarkField( pos: ( Int, Int ) ) = grid( pos._1 )( pos._2 ).marked = false
+
+    //setup the mine grid with, i.e. initialize all fields.
+    private def populateField() {
+        //place mines first...
+        placeMines()
+        //...then fill the rest and calculate no. of adjacent mines for fields
+        for ( i <- 0 until ysize; j <- 0 until xsize ) {
+            //make sure no double placements happen (and mines get overwritten)
+            if ( fieldEmpty( i, j ) ) {
+                grid( i )( j ) = MineField( adjacent = countAdjacentMines( i, j ) )
+            }
+        }
+    }
+
+    //Position mines randomly on the grid. Avoid initial position
+    private def placeMines() {
+        var placed = 0
+        var randy = new Random()
+        while ( placed != minecount ) {
+            val row = randy.nextInt( ysize )
+            val col = randy.nextInt( xsize )
+            //make sure same field doesn't get populated twice
+            if ( fieldEmpty( row, col ) && ( row, col ) != initial_field ) {
+                grid( row )( col ) = MineField( armed = true )
+                placed += 1
+            }
+        }
+    }
+
+    //Count the number of Mines adjacent to a field
+    private def countAdjacentMines( y: Int, x: Int ): Int = getAdjacentPos( y, x ) count ( isMineField )
+
+    //get adjacent, valid positions to a field
+    private def getAdjacentPos( y: Int, x: Int ): List[ ( Int, Int ) ] = {
+        List(
+            ( y + 1, x ),
+            ( y + 1, x - 1 ),
+            ( y, x - 1 ),
+            ( y - 1, x - 1 ),
+            ( y - 1, x ),
+            ( y - 1, x + 1 ),
+            ( y, x + 1 ),
+            ( y + 1, x + 1 ) ).filter( isValidPos )
+    }
+
+    //check whether the field is an armed mine
+    private def isMineField( pos: ( Int, Int ) ): Boolean = grid( pos._1 )( pos._2 ).armed
+
+    //Check if position is within boundaries
+    private def isValidPos( pos: ( Int, Int ) ): Boolean = !fieldEmpty( pos._1, pos._2 ) && yboundaries.contains( pos._1 ) && xboundaries.contains( pos._2 )
+
+    //check whether the position has already been populated with a field
+    private def fieldEmpty( y: Int, x: Int ): Boolean = grid( y )( x ) == null
+
+    private var grid = Array.ofDim[ MineField ]( ysize, xsize )
+
+    private val xboundaries = 0 to xsize
+
+    private val yboundaries = 0 to ysize
 }
 
-case class MineGridException(s: String) extends Exception
+case class MineGridException( s: String ) extends Exception
--- minesweeper/test/de/fhkoeln/minesweeper/model/MineFieldTest.scala
+++ minesweeper/test/de/fhkoeln/minesweeper/model/MineFieldTest.scala
@@ -3,97 +3,99 @@ package de.fhkoeln.minesweeper.model
 import org.specs._
 
 class MineFieldTest extends SpecificationWithJUnit {
-	
-  "An empty unmarked covered MineField" should {
-	  
-    val field = MineField()
-	
-    "have no adjacent Mines" in {
-	    field.adjacent must be_==(0)
-	  }
-	  
-	  "be not armed" in {
-	    field.armed must beFalse
-	  }
-	  
-	"be not marked" in {
-	  field.marked must beFalse
-	}
-	
-	"be covered" in {
-	  field.uncovered must beFalse
-	}
-	
-	"be marked" in {
-	  field.marked = true
-	  field.marked must beTrue
-	}
-	
-	"be uncovered" in {
-	  field.uncover()
-	  field.uncovered must beTrue
-	}
-	}
-  
-  "An empty marked covered MineField" should {
-    val field = MineField(_marked = true)
-    
-    "be marked" in {
-      field.marked must beTrue
+
+    "An empty unmarked covered MineField" should {
+
+        val field = MineField()
+
+        "have no adjacent Mines" in {
+            field.adjacent must be_==( 0 )
+        }
+
+        "be not armed" in {
+            field.armed must beFalse
+        }
+
+        "be not marked" in {
+            field.marked must beFalse
+        }
+
+        "be covered" in {
+            field.uncovered must beFalse
+        }
+
+        "be marked" in {
+            field.marked = true
+            field.marked must beTrue
+        }
+
+        "be uncovered" in {
+            field.uncover()
+            field.uncovered must beTrue
+        }
     }
-    
-    "be not marked" in {
-      field.marked = false
-      field.marked must beFalse
-    }
-    
-    "throw an Exception when " in {
-      field.uncover() must throwA[IllegalArgumentException]
-    }
+
+    "An empty marked covered MineField" should {
         
-  }
-  
-  "An empty uncovered MineField" should {
-    
-    val field = MineField(_uncovered = true)
-    
-    "be uncovered" in {
-      field.uncovered must beTrue
+        val field = MineField( _marked = true )
+
+        "be marked" in {
+            field.marked must beTrue
+        }
+
+        "be not marked" in {
+            field.marked = false
+            field.marked must beFalse
+        }
+
+        "throw an Exception when " in {
+            field.uncover() must throwA[ IllegalArgumentException ]
+        }
+
     }
-    
-    "throw an Exception when getting marked" in {
-      (field.marked = true) must throwA[IllegalArgumentException]
+
+    "An empty uncovered MineField" should {
+
+        val field = MineField( _uncovered = true )
+
+        "be uncovered" in {
+            field.uncovered must beTrue
+        }
+
+        "throw an Exception when getting marked" in {
+            ( field.marked = true ) must throwA[ IllegalArgumentException ]
+        }
     }
-  }
-  
-  "An armed Minefield" should {
-    val field = MineField(armed = true)
-    
-    "should not be able to be constructed with adjacents" in {
-      MineField(2, true) must throwA[IllegalArgumentException]
+
+    "An armed Minefield" should {
+        
+        val field = MineField( armed = true )
+
+        "should not be able to be constructed with adjacents" in {
+            MineField( 2, true ) must throwA[ IllegalArgumentException ]
+        }
+
+        "be marked" in {
+            field.marked = true
+            field.marked must beTrue
+        }
+
+        "be uncovered" in {
+            field.uncover()
+            field.uncovered must beTrue
+        }
     }
-    
-    "be marked" in {
-      field.marked = true
-      field.marked must beTrue
+
+    "An unarmed MineField with adjacent Mines" should {
+
+        val field = new MineField( 3 )
+
+        "not be armed" in {
+            field.armed must beFalse
+        }
+
+        "not allow negative values" in {
+            MineField( -2 ) must throwA[ IllegalArgumentException ]
+        }
     }
-    
-    "be uncovered" in {
-      field.uncover()
-      field.uncovered must beTrue
-    }
-  }
-  
-  "An unarmed MineField with adjacent Mines" should {
-    
-    val field = new MineField(3)
-    
-    "not be armed" in {
-      field.armed must beFalse
-    }
-    
-    "not allow negative values" in {
-      MineField(-2) must throwA[IllegalArgumentException]
-    }
-  }
 }
