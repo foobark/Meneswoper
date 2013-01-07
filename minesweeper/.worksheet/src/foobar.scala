bc3a94d64ec825628f34fdbcb6316f311df3b6cc
ti613 <nightmare666_online@yahoo.de>
2013-01-07 15:38:03 +0100

    emergency

--- minesweeper/.worksheet/src/foobar.scala
+++ minesweeper/.worksheet/src/foobar.scala
@@ -1,1 +1,3 @@
-
+object foobar {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(59); 
+  println("Welcome to the Scala worksheet")}
+}
