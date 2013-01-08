fdd986aef32094b7c6c270c7d4e0adae9fd15b3a
ti614 <ti614@LWIP13.GM-AD.GM.FH-Koeln.DE>
2013-01-08 14:07:25 +0100

    fuck git

--- minesweeper/src/Minesweeper.scala
+++ minesweeper/src/Minesweeper.scala
@@ -8,7 +8,6 @@ object Minesweeper {
   def main(args: Array[String]) {
   val controller = new GridController
   val tui = new TuiReactor(controller)
-  val gui = new GuiReactor(controller)
   
     while (tui.processInputLine(readLine())) {}
   }
