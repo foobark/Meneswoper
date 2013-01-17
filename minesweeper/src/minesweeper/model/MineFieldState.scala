faf8691ef56e9c38ceb7aab3f41f10cd8ebefb14
daniel <nightmare666_online@yahoo.de>
2013-01-17 20:48:29 +0100

    Made subclasses non case classes, see previous commit

--- minesweeper/src/minesweeper/model/MineFieldState.scala
+++ minesweeper/src/minesweeper/model/MineFieldState.scala
@@ -1,6 +1,6 @@
 package minesweeper.model
 
-abstract case class MineFieldState(val adjacent: Int) {
+abstract case class MineFieldState( val adjacent: Int ) {
     def uncovered: Boolean = false
     def marked: Boolean = false
     def triggered: Boolean = false
@@ -8,28 +8,36 @@ abstract case class MineFieldState(val adjacent: Int) {
 }
 
 object MineFieldState {
-    
-    case class covered(override val adjacent: Int = 0) extends MineFieldState(adjacent) {
-        override def covered:Boolean = true
-        
+
+    class covered( override val adjacent: Int = 0 ) extends MineFieldState( adjacent ) {
+        override def covered: Boolean = true
+
         override def toString() = "H"
     }
-    
-    case class marked(override val adjacent: Int = 0) extends MineFieldState(adjacent) {
-        override def marked:Boolean = true
-        
+
+    object covered { def apply( adjacent: Int = 0): covered = new covered( adjacent ) }
+
+    class marked( override val adjacent: Int = 0 ) extends MineFieldState( adjacent ) {
+        override def marked: Boolean = true
+
         override def toString() = "#"
     }
-    
-    case class triggered extends MineFieldState(0) {
-        override def triggered:Boolean = true
-        
+
+    object marked { def apply( adjacent: Int = 0): marked = new marked( adjacent ) }
+
+    class triggered extends MineFieldState( 0 ) {
+        override def triggered: Boolean = true
+
         override def toString() = "*"
     }
-    
-    case class uncovered(override val adjacent: Int = 0) extends MineFieldState(adjacent) {
-        override def uncovered:Boolean = true
-        
+
+    object triggered { def apply(): triggered = new triggered() }
+
+    class uncovered( override val adjacent: Int = 0 ) extends MineFieldState( adjacent ) {
+        override def uncovered: Boolean = true
+
         override def toString() = adjacent.toString
-    }   
+    }
+
+    object uncovered { def apply( adjacent: Int = 0): uncovered = new uncovered( adjacent ) }
 }
