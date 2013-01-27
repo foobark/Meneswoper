5257aa8962ee2c0f1c4b8ddde25a8ad2c351874f
daniel <nightmare666_online@yahoo.de>
2013-01-27 17:57:21 +0100

    added MineFieldState type and Companion Object

--- minesweeper/src/minesweeper/controller/package.scala
+++ minesweeper/src/minesweeper/controller/package.scala
@@ -3,4 +3,6 @@ package minesweeper
 package object controller {
 	type GridState = model.GridState
 	type GridDifficulty = (Int, Int, Int)
+	type MineFieldState = model.MineFieldState
+	val MineFieldState = model.MineFieldState
 }
