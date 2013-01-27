85b4a6baf3f67604eeabc0a66df837dded24106b
daniel <nightmare666_online@yahoo.de>
2013-01-27 18:02:37 +0100

    GridController made into a trait only defining the interface

--- minesweeper/src/minesweeper/controller/GridController.scala
+++ minesweeper/src/minesweeper/controller/GridController.scala
@@ -1,76 +1,18 @@
 package minesweeper.controller
 
-import scala.collection.mutable.LinkedList
-import minesweeper.model.MineFieldGrid
-import minesweeper.model.MineFieldState
-
-class GridController {
-
-    private var reactors = List[ GridReactor ]()
-    private var grid: MineFieldGrid = null
-    private var newgame = true
-    private var difficulty = Difficulties.easy
-    private var xsize = difficulty._1
-    private var ysize = difficulty._2
-    private var xboundaries = 0 until xsize
-    private var yboundaries = 0 until ysize
-
-    def register( reactor: GridReactor ) = {
-        require( reactor != null )
-        reactors = reactors :+ reactor
-    }
-
-    protected def update( event: GridEvent ) = {
-        reactors foreach ( _.react( event ) )
-    }
-
-    def startNewGame( diff: GridDifficulty = Difficulties.easy ) = {
-        require( difficulty != null )
-        difficulty = diff
-        xsize = difficulty._2
-        ysize = difficulty._1
-        yboundaries = 0 until ysize
-        xboundaries = 0 until xsize
-        grid = null
-        newgame = true
-        grid = MineFieldGrid(ysize, xsize)
-        update( new NewGameStarted( grid.getGridState ) )
-    }
-
-    def uncoverPosition( y: Int, x: Int ) = {
-        boundaryCheck( y, x )
-        if ( newgame ) {
-            val clearFields = (y, x) :: grid.getAdjacentPos(y, x)
-            grid = MineFieldGrid( ysize, xsize, difficulty._3,   clearFields)
-            newgame = false
-        }
-        val result = grid.uncoverField( y, x )
-        val newgrid = result._1
-        val lost = result._2
-        val won = result._3
-        if ( lost ) update( new GameLost( newgrid ) )
-        else {
-            if ( won ) update( new GameWon( newgrid ) )
-            else update( new FieldUncovered( newgrid, ( y, x ) ) )
-        }
-
-    }
-
-    def markPosition( y: Int, x: Int ) = {
-        boundaryCheck( y, x )
-        if ( !newgame ) {
-            grid.markField( ( y, x ) )
-            update( new FieldMarked( grid.getGridState(), ( y, x ) ) )
-        }
-    }
-
-    def unmarkPosition( y: Int, x: Int ) = {
-        boundaryCheck( y, x )
-        if ( !newgame ) {
-            grid.unmarkField( ( y, x ) )
-            update( new FieldUnmarked( grid.getGridState(), ( y, x ) ) )
-        }
-    }
-
-    private def boundaryCheck( y: Int, x: Int ) = require( ( yboundaries contains y ) && ( xboundaries contains x ) )
+trait GridController {
+    
+    def register(reactor: GridReactor)
+    
+    def unregister(reactor: GridReactor)
+    
+    def startNewGame(diff: GridDifficulty)
+    
+    def uncoverPosition(y: Int, x: Int)
+    
+    def markPosition(y: Int, x: Int)
+    
+    def unmarkPosition(y: Int, x: Int)
+    
+	protected def update( event: GridEvent )
 }
